2bd3e9f37f2fc31f10560dc0e13c3e31b4cd2c33
binhsonnguyen <son@binhsonnguyen.com>
2018-08-20 10:15:03 +0700

    test buzz

--- src/test/java/game/FizzBuzzPlayerTest.java
+++ src/test/java/game/FizzBuzzPlayerTest.java
@@ -6,12 +6,20 @@ import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzPlayerTest {
     private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
 
     @Test
     public void testFizz() {
         final int input = 3;
         String actual = FizzBuzzPlayer.answer(input);
         assertEquals(FIZZ, actual);
+    }
+
+    @Test
+    public void testBuzz() {
+        final int input = 5;
+        String actual = FizzBuzzPlayer.answer(input);
+        assertEquals(BUZZ, actual);
     }
 
 }
