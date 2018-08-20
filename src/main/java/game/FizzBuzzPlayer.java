a9f2f7c3c1f61d34287b2a4994771ec8e6243cc2
binhsonnguyen <son@binhsonnguyen.com>
2018-08-20 10:22:04 +0700

    refacgtor player class

--- src/main/java/game/FizzBuzzPlayer.java
+++ src/main/java/game/FizzBuzzPlayer.java
@@ -1,13 +1,16 @@
 package game;
 
 public class FizzBuzzPlayer {
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+
     public static String answer(int number) {
         if (isFizz(number)) {
-            return "Fizz";
+            return FIZZ;
         } else if (isBuzz(number)) {
-            return "Buzz";
+            return BUZZ;
         }
-        return String.valueOf(number);
+        return normalAnswer(number);
     }
 
     private static boolean isFizz(int number) {
@@ -16,5 +19,9 @@ public class FizzBuzzPlayer {
 
     private static boolean isBuzz(int number) {
         return number == 5;
+    }
+
+    private static String normalAnswer(int number) {
+        return String.valueOf(number);
     }
 }
